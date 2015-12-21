49806d85e78a8de19b2fe81c603020c03e3bace8
daviddiehl <davidfdiehl@gmail.com>
2015-12-20 19:45:29 -0800

    tenth exercise

---
+++ 10-relational-operator/src/RelationalOperators.java
@@ -0,0 +1,12 @@
+// relational operators will always return a boolean value of true or false
+// '<'  less than
+// '<=' less than or equal to
+// '>'  greater than
+// '>=' greater than or equal to
+
+public class RelationalOperators {
+	public static void main(String[] args) {
+
+		System.out.println(0 < 1);
+	}
+}
