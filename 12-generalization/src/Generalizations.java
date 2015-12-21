46cb2429e063ca2a1a540cf74f4c48d29b6adc67
daviddiehl <davidfdiehl@gmail.com>
2015-12-20 20:19:49 -0800

    twelfth exercise

---
+++ 12-generalization/src/Generalizations.java
@@ -0,0 +1,16 @@
+// data types: int, boolean, and char
+// arithmetic operators: +, -, *, /, and %
+// relational operators: <, <=, >, and >=
+// equality operators: == and !=
+
+public class Generalizations {
+	public static void main(String[] args) {
+
+		boolean isComplete = true;
+		int awesomeLevel = 121;
+		int epicLevel = 2 * awesomeLevel;
+		System.out.println(epicLevel);
+
+	}
+
+}
