b60e9844ae9e214aab49c2b16f948e220141008c
daviddiehl <davidfdiehl@gmail.com>
2015-12-20 16:36:37 -0800

    third exercise

---
+++ 3-data-type-boolean/src/DataTypesB.java
@@ -0,0 +1,8 @@
+// Boolean data type can only be either true or false
+// it seems that it has to be lowercase ("True" does not equal "true"
+
+public class DataTypesB {
+	public static void main(String[] args) {
+		System.out.println(true);
+	}
+}
