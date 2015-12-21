189d9345c0c899e7b53c0b82d1e5637a07308830
daviddiehl <davidfdiehl@gmail.com>
2015-12-20 16:28:35 -0800

    second exercise

---
+++ 2-data-type-int/src/DataTypes.java
@@ -0,0 +1,9 @@
+// integer data type allows values between -2,147,483,648 and 2,147,483,647
+// integer are all positive and negative numbers including zero
+
+public class DataTypes {
+	public static void main(String[] args) {
+
+		System.out.println(6);
+	}
+}
