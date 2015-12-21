f96a48393ec419fea38a6f1df996532415c7b919
daviddiehl <davidfdiehl@gmail.com>
2015-12-20 16:45:29 -0800

    fourth exercise

---
+++ 4-data-type-char/src/DataTypesC.java
@@ -0,0 +1,9 @@
+// the char data type represents single characters
+// all values must be in single quotes like this: 'D'
+
+public class DataTypesC {
+	public static void main(String[] args) {
+		
+		System.out.println('D');
+	}
+}
